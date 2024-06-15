2d0fadc1b9718c1ede2593851eb8e6c020495e67
dontebailey <baileydonte27@gmail.com>
2024-06-15 17:02:42 -0400

    created submit form for todo items

---
+++ src/components/Todo.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          type="text"
+        ></input>
+        <button type="submit">Add</button>
+      </form>
+      {console.log(todos)}
+    </div>
+  );
+}
