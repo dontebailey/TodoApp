113b633a041a9ad387dbe903872bc9114ca4800c
dontebailey <baileydonte27@gmail.com>
2024-07-05 16:54:50 -0400

    added completed task functionality and marking items as completed

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,10 +4,23 @@ export default function TodoItem({ item, todos, setTodos }) {
     console.log("Delete button clicked for item", item);
     setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function handleClick(name) {
+    setTodos(
+      todos.map((todo) =>
+        todo.name === name ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  }
+  const className = item.done ? styles.completed : "";
+
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}
+        <span className={className} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
+
         <span>
           <button
             onClick={() => handleDelete(item)}
